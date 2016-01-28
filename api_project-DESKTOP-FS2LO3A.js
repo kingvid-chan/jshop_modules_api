b615eb0b19946947a9bc15fe5479816c04d7aeac
kingvid-chan <kingvid.chan@gmail.com>
2016-01-28 17:49:16 +0800

    add

---
+++ api_project-DESKTOP-FS2LO3A.js
@@ -0,0 +1,15 @@
+define({
+  "name": "jShop Modules",
+  "version": "0.0.0",
+  "description": "jshop模块加载逻辑介绍文档: <a href='http://kingvid-chan.github.io/jshop-module-logic/' target='_blank'>http://kingvid-chan.github.io/jshop-module-logic/</a>",
+  "title": "API for jShop Module",
+  "url": "",
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2016-01-23T14:24:18.626Z",
+    "url": "http://apidocjs.com",
+    "version": "0.14.0"
+  }
+});
